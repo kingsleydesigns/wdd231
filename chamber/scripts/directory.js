1a047768c09b29d302b14e5d008c19a5b903441d
kingsleydesigns <ezepuekingsley@gmail.com>
2025-01-23 05:05:16 +0100

    updated directory page design

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -17,6 +17,21 @@ menubutton.addEventListener('click', () => {
 	menubutton.classList.toggle('show');
 });
 
+// Select all links inside the nav
+const navLinks = document.querySelectorAll("nav ul li a");
+
+// Add a click event listener to each link
+navLinks.forEach(link => {
+  link.addEventListener("click", function () {
+    // Remove the 'active' class from all nav links
+    navLinks.forEach(nav => nav.parentElement.classList.remove("active"));
+
+    // Add the 'active' class to the clicked link's parent <li>
+    this.parentElement.classList.add("active");
+  });
+});
+
+
 // Main section
 const cards = document.querySelector('#members');
 const path = './data/members.json';
